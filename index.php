f48a41f16e487490110a5c6d00b2c10643022b25
andrea-livornese <andrealivornese@gmail.com>
2024-03-15 15:24:09 +0100

    creazione della grafica

--- index.php
+++ index.php
@@ -4,26 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-badwords</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 
-    <h1>
-        Elaboratore di testo
-    </h1>
+    <div class="container">
+       
 
-    <form action="testoElaborato.php">
-        
-        <label for="text-area">Inserisci qui del testo</label>
-        <textarea name="paragrafo" id="text-area" cols="20" rows="3">
+        <h1 class="text-center mb-5">
+            Elaboratore di testo
+        </h1>
+    
 
-        </textarea>
+        <div class="row justify-content-center ">
 
-        <label for="testo-censurato">Inserisci la parola da censurare</label>
-        <input type="text" id="testo-censurato" name="censure" required>
+            <div class="col-3">
 
-        <input type="submit" value="Elabora">
+                <form action="testoElaborato.php">
+                    
+                    <label for="text-area">Inserisci qui del testo</label>
+                    <textarea name="paragrafo" class="form-control mb-4" id="text-area">
+            
+                    </textarea>
+            
+                    <label for="testo-censurato">Inserisci la parola da censurare</label>
+                    <input type="text" class="form-control mb-4" id="testo-censurato" name="censure" required>
+            
+                    <input type="submit" class="btn btn-danger" value="Elabora">
+            
+                </form>
 
-    </form>
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     
 </body>
 </html>
--- testoElaborato.php
+++ testoElaborato.php
@@ -11,11 +11,12 @@ $censura = $_GET['censure'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>text-censured</title>
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <h1>
-        Elaboratore di testo
-    </h1>
+
 
     <?php
     
@@ -24,13 +25,60 @@ $censura = $_GET['censure'];
     $array_paragrafo = str_ireplace($censura,"***", $array_paragrafo);
 
     $result = implode(" ",$array_paragrafo);
+
     ?>
 
 
-    <p>
-        <?php echo $result ?>
-    </p>
 
+    <div class="container">
+        
+
+        <h1 class="text-center mb-5">
+            Elaboratore di testo
+        </h1>
+    
+
+        <div class="row justify-content-center ">
+
+            <div class="col-3">
+
+            <div class="par-wrapper mb-4 bg-body-tertiary rounded p-3">
+                <p>
+                    <?php echo $paragrafo ?>
+                </p>
+                <div>
+                    La stringa è lunga <?php echo strlen($paragrafo); ?> lettere.
+                </div>
+            </div>
+
+
+            <div class="par-wrapper bg-success rounded p-3 text-white">
+                <p>
+                    <?php echo $result ?>
+                </p>
+
+                <div>
+                    La stringa censurata è lunga <?php echo strlen($result); ?> lettere.
+                </div>
+            </div>
+
+            </div>
+
+
+        </div>
+
+
+    </div>
+
+
+
+
+
+    
+
+
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     
 </body>
