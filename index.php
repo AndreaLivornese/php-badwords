9ce52e6392ba3cf9e3ebe351bcf287fb767feead
andrea-livornese <andrealivornese@gmail.com>
2024-03-15 14:39:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+    <form action="">
+
+        <textarea name="paragrafo" id="" cols="30" rows="5">
+
+        </textarea>
+
+        <input type="text" name="censure" required>
+
+    </form>
+    
+</body>
+</html>
